92dafdb195305350a5a5579990e5fdae5cc49817
Suo Caiyuan <caiyuan.suo@mathartsys.com>
2019-02-12 09:19:53 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# runbird-leetCode" 
---
+++ src/com/scy/AddTwoNumbers.java
@@ -0,0 +1,89 @@
+package com.scy;
+
+/**
+ * 描述:
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * <p>
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * <p>
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 示例：
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ * @author suocaiyuan
+ * @version V1.0
+ */
+public class AddTwoNumbers {
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(2);
+        n1.setNext(new ListNode(4));
+        n1.getNext().setNext(new ListNode(3));
+
+        ListNode n2 = new ListNode(5);
+        n2.setNext(new ListNode(6));
+        n2.getNext().setNext(new ListNode(4));
+
+        ListNode listNode = addTwoNumbers(n1, n2);
+        System.out.println(listNode.toString());
+    }
+}
+
+class ListNode {
+
+    int val;
+
+    ListNode next;
+
+    ListNode(int x) {
+        this.val = x;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
