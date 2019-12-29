af9b60d717c78b7b9fbc47d6af9f9f496e78b231
suocaiyuan <suocaiyuan@163.com>
2019-12-29 23:29:30 +0800

    done for leetcode 307

---
+++ src/com/scy/algorithm/RangeSumQueryMutable.java
@@ -0,0 +1,68 @@
+package com.scy.algorithm;
+
+/**
+ * @desc: 给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
+ *
+ * update(i, val) 函数可以通过将下标为 i 的数值更新为 val，从而对数列进行修改。
+ *
+ * 示例:
+ *
+ * Given nums = [1, 3, 5]
+ *
+ * sumRange(0, 2) -> 9
+ * update(1, 2)
+ * sumRange(0, 2) -> 8
+ * 说明:
+ *
+ * 数组仅可以在 update 函数下进行修改。
+ * 你可以假设 update 函数与 sumRange 函数的调用次数是均匀分布的。
+ *
+ * 来源：力扣（LeetCode）307
+ * 链接：https://leetcode-cn.com/problems/range-sum-query-mutable
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author: Suocaiyuan
+ * @date: 2019-12-29 23:10
+ **/
+public class RangeSumQueryMutable {
+
+    private int[] sum;
+
+    private int[] data;
+    /**
+     * Your NumArray object will be instantiated and called as such:
+     * NumArray obj = new NumArray(nums);
+     * obj.update(i,val);
+     * int param_2 = obj.sumRange(i,j);
+     */
+    public RangeSumQueryMutable(int[] nums) {
+        data = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            data[i] = nums[i];
+        }
+
+        sum = new int[nums.length + 1];
+        sum[0] = 0;
+        for (int i = 1; i < sum.length; i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
+        }
+    }
+
+    public void update(int i, int val) {
+        data[i] = val;
+        for (int j = i+1; j < sum.length; j++) {
+            sum[j] = sum[j - 1] + data[j - 1];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5};
+        RangeSumQueryMutable mutable = new RangeSumQueryMutable(nums);
+        System.out.println(mutable.sumRange(0, 2));
+        mutable.update(1, 2);
+        System.out.println(mutable.sumRange(0, 2));
+    }
+}
