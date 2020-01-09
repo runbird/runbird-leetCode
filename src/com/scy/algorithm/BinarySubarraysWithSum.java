dd66bdc4378e75431253307742c8d8c5d42e2c48
suocaiyuan <suocaiyuan@163.com>
2020-01-09 22:23:46 +0800

    done for leetcode 122

---
+++ src/com/scy/algorithm/BinarySubarraysWithSum.java
@@ -0,0 +1,32 @@
+package com.scy.algorithm;
+
+/**
+ * 类名： BinarySubarraysWithSum <br>
+ * 描述：在由若干 0 和 1  组成的数组 A 中，有多少个和为 S 的非空子数组。
+ *
+ * 示例：
+ * 输入：A = [1,0,1,0,1], S = 2
+ * 输出：4
+ * 解释：
+ * 如下面黑体所示，有 4 个满足题目要求的子数组：
+ * [1,0,1..]
+ * [1,0,1,0]
+ * [0,1,0,1]
+ * [..1,0,1]
+ *  
+ *
+ * 提示：
+ * A.length <= 30000
+ * 0 <= S <= A.length
+ * A[i] 为 0 或 1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-subarrays-with-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 <br>
+ * 创建日期： 2020/1/2 <br>
+ *
+ * @author suocaiyuan
+ * @version V1.0
+ */
+public class BinarySubarraysWithSum {
+}
--- src/com/scy/algorithm/RemoveDuplicatesFromSortedArray.java
+++ src/com/scy/algorithm/RemoveDuplicatesFromSortedArray.java
@@ -41,16 +41,16 @@ public class RemoveDuplicatesFromSortedArray {
         int j = 0;
         for (int i = 1; i < nums.length; i++) {
             if (nums[j] != nums[i]) {
-                j ++;
+                j++;
                 nums[j] = nums[i];
             }
         }
-        return j+1;
+        return j + 1;
     }
 
     public static void main(String[] args) {
         RemoveDuplicatesFromSortedArray array = new RemoveDuplicatesFromSortedArray();
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
         int i = array.removeDuplicates(nums);
         System.out.println(i);
     }
