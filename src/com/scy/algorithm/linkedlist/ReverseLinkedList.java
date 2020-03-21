c4761ffef6492ed519f3d4026f728e5d6eec2d7c
suocaiyuan <suocaiyuan@163.com>
2020-03-21 13:12:09 +0800

    LinkedList

--- src/com/scy/algorithm/linkedlist/ReverseLinkedList.java
+++ src/com/scy/algorithm/linkedlist/ReverseLinkedList.java
@@ -20,6 +20,11 @@ import com.scy.algorithm.datastructure.array.ListNode;
  **/
 public class ReverseLinkedList {
 
+    /**
+     * 双指针法
+     * @param head
+     * @return
+     */
     public ListNode reverseList(ListNode head) {
         if (head == null) return null;
         ListNode cur = head;
@@ -32,5 +37,36 @@ public class ReverseLinkedList {
             cur = temp;
         }
         return cur;
+    }
+
+    /**
+     * 递归法
+     * https://leetcode-cn.com/problems/reverse-linked-list/solution/dong-hua-yan-shi-206-fan-zhuan-lian-biao-by-user74/
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        //这里的cur就是最后一个节点
+        ListNode cur = reverseList2(head.next);
+        //如果链表是 1->2->3->4->5，那么此时的cur就是5
+        //而head是4，head的下一个是5，下下一个是空
+        //所以head.next.next 就是5->4
+        head.next.next = head;
+        //防止链表循环，需要将head.next设置为空
+        head.next = null;
+        //每层递归函数都返回cur，也就是最后一个节点
+        return cur;
+    }
+
+
+        public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        ListNode listNode = new ListNode(arr);
+        ReverseLinkedList reverse = new ReverseLinkedList();
+        ListNode result = reverse.reverseList(listNode);
+        System.out.println(result.toString());
     }
 }
