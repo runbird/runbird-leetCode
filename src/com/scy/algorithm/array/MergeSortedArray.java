c7acb171a898891c9b6558599061bf5a88319102
suocaiyuan <suocaiyuan@163.com>
2020-06-23 21:29:18 +0800

    merge sorted array

--- src/com/scy/algorithm/array/MergeSortedArray.java
+++ src/com/scy/algorithm/array/MergeSortedArray.java
@@ -38,17 +38,35 @@ public class MergeSortedArray {
         int len1 = m - 1;
         int len2 = n - 1;
         int len = m + n - 1;
-        while (len1 > 0 && len2 > 0) {
+        while (len1 >= 0 && len2 >= 0) {
             nums1[len--] = nums2[len2] >= nums1[len1] ? nums2[len2--] : nums1[len1--];
         }
         // 表示将nums2数组从下标0位置开始，拷贝到nums1数组中，从下标0位置开始，长度为len2+1
         System.arraycopy(nums2, 0, nums1, 0, len2 + 1);
     }
 
+    public void merge3(int[] nums1, int m, int[] nums2, int n) {
+        int[] copy = new int[m];
+        System.arraycopy(nums1, 0, copy, 0, m);
+
+        int copyPointer = 0, nums2Pointer = 0, nums1Pointer = 0;
+
+        while ((copyPointer < m) && (nums2Pointer < n)) {
+           nums1[nums1Pointer++] = copy[copyPointer] > nums2[nums2Pointer] ? nums2[nums2Pointer++] : copy[copyPointer++];
+        }
+
+        if (copyPointer < m){
+            System.arraycopy(copy, copyPointer, nums1, copyPointer + nums2Pointer, m + n - nums2Pointer - copyPointer);
+        }
+        if (nums2Pointer < n) {
+            System.arraycopy(nums2, nums2Pointer, nums1, copyPointer + nums2Pointer, m + n - nums1Pointer - copyPointer);
+        }
+    }
+
     public static void main(String[] args) {
         int[] sum1 = {1, 2, 3, 0, 0, 0}, sum2 = {2, 5, 6};
         int m = 3, n = 3;
         MergeSortedArray msa = new MergeSortedArray();
-        msa.merge2(sum1, m, sum2, n);
+        msa.merge1(sum1, m, sum2, n);
     }
 }
