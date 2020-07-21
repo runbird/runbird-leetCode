823025b05661db3c475bff291e384aaee15af37d
suocaiyuan <suocaiyuan@163.com>
2020-07-21 16:46:18 +0800

    leetcode-695

---
+++ src/com/scy/algorithm/array/MaxAreaOfIsland.java
@@ -0,0 +1,78 @@
+package com.scy.algorithm.array;
+
+import java.util.Arrays;
+
+/**
+ * 类名： MaxAreaOfIsland <br>
+ * 描述：给定一个包含了一些 0 和 1 的非空二维数组 grid 。
+ * 一个 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在水平或者竖直方向上相邻。
+ * 你可以假设 grid 的四个边缘都被 0（代表水）包围着。
+ * 找到给定的二维数组中最大的岛屿面积。(如果没有岛屿，则返回面积为 0 。)
+ *
+ * 示例 1:
+ * [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ *  [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ *  [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ *  [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ *  [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ *  [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ *  [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ *  [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * 对于上面这个给定矩阵应返回 6。注意答案不应该是 11 ，因为岛屿只能包含水平或垂直的四个方向的 1 。
+ *
+ * 示例 2:
+ * [[0,0,0,0,0,0,0,0]]
+ * 对于上面这个给定的矩阵, 返回 0。
+ *
+ * 来源：力扣（LeetCode）695
+ * 链接：https://leetcode-cn.com/problems/max-area-of-island
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 <br>
+ * 创建日期： 2020/7/21 <br>
+ *
+ * @author suocaiyuan
+ * @version V1.0
+ */
+public class MaxAreaOfIsland {
+    private int n = 0, m = 0;
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int res = 0;
+        n = grid.length ;
+        m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    res = Math.max(res, dfs(i, j, grid));
+                }
+            }
+        }
+        return res;
+    }
+
+    private int dfs(int i, int j, int[][] grid) {
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == 0) return 0;
+        grid[i][j] = 0;
+        int num = 1;
+        num += dfs(i - 1, j, grid);
+        num += dfs(i + 1, j, grid);
+        num += dfs(i, j - 1, grid);
+        num += dfs(i, j + 1, grid);
+        return num;
+    }
+
+    public static void main(String[] args) {
+        //int[][] nums = new int[][]{{0,0,0,0,0,0,0,0}};
+        int[][] nums = new int[][]{{1, 1, 0, 0, 0}, {1, 1, 0, 0, 0}, {0, 0, 0, 1, 1}, {0, 0, 0, 1, 1}};
+//        int[][] nums = new int[][]
+//                {{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+//                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+//                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
+        MaxAreaOfIsland mai = new MaxAreaOfIsland();
+        System.out.println(mai.maxAreaOfIsland(nums));
+    }
+}
