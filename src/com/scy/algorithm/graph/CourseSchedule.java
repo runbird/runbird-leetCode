bd3ba5819c0d4f5bba74209ccc77dbfd2b5bd183
suocaiyuan <suocaiyuan@163.com>
2020-08-04 20:41:19 +0800

    leetcode-207

---
+++ src/com/scy/algorithm/graph/CourseSchedule.java
@@ -0,0 +1,86 @@
+package com.scy.algorithm.graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 类名： CourseSchedule <br>
+ * 描述：你这个学期必须选修 numCourse 门课程，记为 0 到 numCourse-1 。
+ * 在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们：[0,1]
+ * 给定课程总量以及它们的先决条件，请你判断是否可能完成所有课程的学习？
+ *
+ * 示例 1:
+ * 输入: 2, [[1,0]]
+ * 输出: true
+ * 解释: 总共有 2 门课程。学习课程 1 之前，你需要完成课程 0。所以这是可能的。
+ *
+ * 示例 2:
+ * 输入: 2, [[1,0],[0,1]]
+ * 输出: false
+ * 解释: 总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0；并且学习课程 0 之前，你还应先完成课程 1。这是不可能的。
+ *  
+ * 提示：
+ * 输入的先决条件是由 边缘列表 表示的图形，而不是 邻接矩阵 。详情请参见图的表示法。
+ * 你可以假定输入的先决条件中没有重复的边。
+ * 1 <= numCourses <= 10^5
+ *
+ * 来源：力扣（LeetCode）207
+ * 链接：https://leetcode-cn.com/problems/course-schedule
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 <br>
+ * 创建日期： 2020/8/4 <br>
+ *
+ * @author suocaiyuan
+ * @version V1.0
+ */
+public class CourseSchedule {
+
+    //方法一：入度表（广度优先遍历）
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegrees = new int[numCourses];
+        List<List<Integer>> adjacency = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++)
+            adjacency.add(new ArrayList<>());
+        // Get the indegree and adjacency of every course.
+        for(int[] cp : prerequisites) {
+            indegrees[cp[0]]++;
+            adjacency.get(cp[1]).add(cp[0]);
+        }
+        // Get all the courses with the indegree of 0.
+        for(int i = 0; i < numCourses; i++)
+            if(indegrees[i] == 0) queue.add(i);
+        // BFS TopSort.
+        while(!queue.isEmpty()) {
+            int pre = queue.poll();
+            numCourses--;
+            for(int cur : adjacency.get(pre))
+                if(--indegrees[cur] == 0) queue.add(cur);
+        }
+        return numCourses == 0;
+    }
+
+    //方法二 深度优先遍历
+    public boolean canFinish2(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adjacency = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++)
+            adjacency.add(new ArrayList<>());
+        int[] flags = new int[numCourses];
+        for(int[] cp : prerequisites)
+            adjacency.get(cp[1]).add(cp[0]);
+        for(int i = 0; i < numCourses; i++)
+            if(!dfs(adjacency, flags, i)) return false;
+        return true;
+    }
+    private boolean dfs(List<List<Integer>> adjacency, int[] flags, int i) {
+        if(flags[i] == 1) return false;
+        if(flags[i] == -1) return true;
+        flags[i] = 1;
+        for(Integer j : adjacency.get(i))
+            if(!dfs(adjacency, flags, j)) return false;
+        flags[i] = -1;
+        return true;
+    }
+
+}
