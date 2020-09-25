c32779f34c3d661ee7b59403b29373de9694f9ca
suocaiyuan <suocaiyuan@163.com>
2020-09-25 17:35:17 +0800

    leetcode-Tree

---
+++ src/com/scy/algorithm/tree/FindModeInBinarySearchTree.java
@@ -0,0 +1,86 @@
+package com.scy.algorithm.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 类名： FindModeInBinarySearchTree <br>
+ * 描述：
+ * 给定一个有相同值的二叉搜索树（BST），找出 BST 中的所有众数（出现频率最高的元素）。
+ *
+ * 假定 BST 有如下定义：
+ *
+ * 结点左子树中所含结点的值小于等于当前结点的值
+ * 结点右子树中所含结点的值大于等于当前结点的值
+ * 左子树和右子树都是二叉搜索树
+ *
+ * 例如：
+ * 给定 BST [1,null,2,2],
+ *
+ *    1
+ *     \
+ *      2
+ *     /
+ *    2
+ * 返回[2].
+ *
+ * 提示：如果众数超过1个，不需考虑输出顺序
+ *
+ * 进阶：你可以不使用额外的空间吗？（假设由递归产生的隐式调用栈的开销不被计算在内） <br>
+ * 来源：力扣（LeetCode）501
+ * 链接：https://leetcode-cn.com/problems/find-mode-in-binary-search-tree/
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author suocaiyuan
+ * @version V1.0
+ */
+public class FindModeInBinarySearchTree {
+
+    int base, count, maxCount;
+    List<Integer> answer = new ArrayList<>();
+
+    public int[] findMode(TreeNode root) {
+        TreeNode cur = root, pre = null;
+        while (cur != null) {
+            if (cur.left == null) {
+                update(cur.val);
+                cur = cur.right;
+                continue;
+            }
+            pre = cur.left;
+            while (pre.right != null && pre.right != cur) {
+                pre = pre.right;
+            }
+            if (pre.right == null) {
+                pre.right = cur;
+                cur = cur.left;
+            } else {
+                pre.right = null;
+                update(cur.val);
+                cur = cur.right;
+            }
+        }
+        int[] mode = new int[answer.size()];
+        for (int i = 0; i < answer.size(); ++i) {
+            mode[i] = answer.get(i);
+        }
+        return mode;
+    }
+
+    public void update(int x) {
+        if (x == base) {
+            ++count;
+        } else {
+            count = 1;
+            base = x;
+        }
+        if (count == maxCount) {
+            answer.add(base);
+        }
+        if (count > maxCount) {
+            maxCount = count;
+            answer.clear();
+            answer.add(base);
+        }
+    }
+}
