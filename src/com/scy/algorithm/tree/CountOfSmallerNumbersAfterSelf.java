ec9b0a4d2003e59909f905dfd9f7b2efae4f9605
suocaiyuan <suocaiyuan@163.com>
2021-08-19 21:48:26 +0800

    merge sort and LRU

--- src/com/scy/algorithm/tree/CountOfSmallerNumbersAfterSelf.java
+++ src/com/scy/algorithm/tree/CountOfSmallerNumbersAfterSelf.java
@@ -1,7 +1,9 @@
 package com.scy.algorithm.tree;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 类名： CountOfSmallerNumbersAfterSelf <br>
@@ -41,9 +43,9 @@ public class CountOfSmallerNumbersAfterSelf {
 
 
     //    链接：https://leetcode-cn.com/problems/count-of-smaller-numbers-after-self/solution/gui-bing-pai-xu-suo-yin-shu-zu-python-dai-ma-java-/
-    private int[] temp;
-    private int[] counter;
-    private int[] indexes;
+    private int[] t;
+    private int[] cnt;
+    private int[] idx;
 
     //归并排序
     public List<Integer> countSmaller2(int[] nums) {
@@ -52,15 +54,15 @@ public class CountOfSmallerNumbersAfterSelf {
         if (len == 0) {
             return res;
         }
-        temp = new int[len];
-        counter = new int[len];
-        indexes = new int[len];
+        t = new int[len];
+        cnt = new int[len];
+        idx = new int[len];
         for (int i = 0; i < len; i++) {
-            indexes[i] = i;
+            idx[i] = i;
         }
         mergeAndCountSmaller(nums, 0, len - 1);
         for (int i = 0; i < len; i++) {
-            res.add(counter[i]);
+            res.add(cnt[i]);
         }
         return res;
     }
@@ -82,7 +84,7 @@ public class CountOfSmallerNumbersAfterSelf {
         mergeAndCountSmaller(nums, mid + 1, r);
         // 归并排序的优化，同样适用于该问题
         // 如果索引数组有序，就没有必要再继续计算了
-        if (nums[indexes[mid]] > nums[indexes[mid + 1]]) {
+        if (nums[idx[mid]] > nums[idx[mid + 1]]) {
             mergeOfTwoSortedArrAndCountSmaller(nums, l, mid, r);
         }
     }
@@ -99,38 +101,78 @@ public class CountOfSmallerNumbersAfterSelf {
     private void mergeOfTwoSortedArrAndCountSmaller(int[] nums, int l, int mid, int r) {
         // 3,4  1,2
         for (int i = l; i <= r; i++) {
-            temp[i] = indexes[i];
+            t[i] = idx[i];
         }
         int i = l;
         int j = mid + 1;
         // 左边出列的时候，计数
         for (int k = l; k <= r; k++) {
             if (i > mid) {
-                indexes[k] = temp[j];
+                idx[k] = t[j];
                 j++;
             } else if (j > r) {
-                indexes[k] = temp[i];
+                idx[k] = t[i];
                 i++;
                 // 此时 j 用完了，[7,8,9 | 1,2,3]
                 // 之前的数就和后面的区间长度构成逆序
-                counter[indexes[k]] += (r - mid);
-            } else if (nums[temp[i]] <= nums[temp[j]]) {
-                indexes[k] = temp[i];
+                cnt[idx[k]] += (r - mid);
+            } else if (nums[t[i]] <= nums[t[j]]) {
+                idx[k] = t[i];
                 i++;
                 // 此时 [4,5, 6   | 1,2,3 10 12 13]
                 //           mid          j
-                counter[indexes[k]] += (j - mid - 1);
+                cnt[idx[k]] += (j - mid - 1);
             } else {
                 // nums[indexes[i]] > nums[indexes[j]] 构成逆序
-                indexes[k] = temp[j];
+                idx[k] = t[j];
                 j++;
             }
         }
     }
 
 
+    //MergeSort排序解决
+    public List<Integer> countSmaller3(int[] nums) {
+        if (nums == null || nums.length == 0) return new ArrayList<>();
+        int n = nums.length;
+        cnt = new int[n];
+        t = new int[n];
+        idx = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            idx[i] = i;
+        }
+
+        msort(nums, idx, 0, n);
+        return Arrays.stream(cnt).boxed().collect(Collectors.toList());
+    }
+
+    private void msort(int[] nums, int[] idx, int b, int e) {
+        if (b >= e || b + 1 >= e) {
+            return;
+        }
+
+        int m = b + ((e - b) >> 1);
+        msort(nums, idx, b, m);
+        msort(nums, idx, m, e);
+
+        int i = b, j = m, to = b;
+        while (i < m || j < e) {
+            if (j >= e || i < m && nums[idx[i]] <= nums[idx[j]]) {
+                cnt[idx[i]] = j - m;
+                t[to++] = idx[i++];
+            } else {
+                t[to++] = idx[j++];
+            }
+        }
+
+        for (i = b; i < e; i++) {
+            idx[i] = t[i];
+        }
+    }
+
     public static void main(String[] args) {
-        int[] arr = {5,2,6,1};
+        int[] arr = {5, 2, 6, 1};
         CountOfSmallerNumbersAfterSelf csnas = new CountOfSmallerNumbersAfterSelf();
         List<Integer> list = csnas.countSmaller(arr);
         list.forEach(System.out::println);
